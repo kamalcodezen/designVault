6c6aecdb702ff3bb270d8c26f4699ccdf45618ba
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-06 19:52:51 +0530

    login ang google login complete

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -3,7 +3,7 @@ import RegisterForm from "@/components/auth/RegisterForm";
 const SignUpPage = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center bg-no-repeat p-"
+      className="min-h-screen bg-cover bg-center bg-no-repeat "
       style={{ backgroundImage: "url('/images/banner-3.png')" }}
     >
       <RegisterForm />
--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,12 +1,223 @@
+"use client";
 
+import "../auth/auth.css";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
 
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
-const LoginForm = () => {
-    return (
-        <div>
-            
+const LoginFrom = () => {
+  const router = useRouter();
+  // const searchParams = useSearchParams();
+
+  // const redirect = searchParams.get("redirect");
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // Google login
+  const googleSignUp = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+
+  // LOGIN
+  const handleLogin = async (data) => {
+    // console.log(data);
+    const email = data?.email?.toLowerCase()?.trim();
+    const password = data?.password?.trim();
+
+    /* =========================
+        VALIDATION
+  ========================= */
+
+    if (
+      password.length < 6 ||
+      !/[A-Z]/.test(password) ||
+      !/[a-z]/.test(password)
+    ) {
+      toast.error(
+        "Password dosen't match",
+      );
+      return;
+    }
+
+    try {
+      const { data: DataRes, error } = await authClient.signIn.email(
+        {
+          email,
+          password,
+        },
+        {
+          onRequest: () => {
+            setLoading(true);
+          },
+
+          onSuccess: () => {
+            setLoading(false);
+            toast.success("Welcome back! 🌸 Login successful", {
+              position: "top-right",
+              autoClose: 3000,
+            });
+            // router.push("/");
+
+            const params = new URLSearchParams(window.location.search);
+            const redirect = params.get("redirect");
+
+            window.location.href = redirect || "/";
+          },
+
+          onError: (ctx) => {
+            setLoading(false);
+            toast.error(ctx.error.message || "Login failed. Please try again", {
+              position: "top-right",
+              autoClose: 4000,
+            });
+          },
+        },
+      );
+      // console.log(DataRes, error);
+    } catch (error) {
+      setLoading(false);
+      toast.error("An unexpected error occurred", {
+        position: "top-right",
+        autoClose: 4000,
+      });
+      // console.log(error);
+    }
+  };
+
+  return (
+    <div className="relative w-11/12 mx-auto min-h-screen flex items-center justify-center ">
+      <div className="min-h-[80vh] flex flex-col items-center justify-center w-full max-w-md py-6 bg-white/[0.03]  backdrop-blur-2xl border border-white/10 rounded shadow-[0_8px_32px_rgba(0,0,0,0.25)] overflow-hidden">
+        <div className="text-start">
+          {/* header */}
+          <p className="font-semibold text-3xl  bg-linear-to-r from-[#15508b] via-[#1c6ab7] to-orange-500 bg-clip-text text-transparent">
+            Welcome Back
+          </p>
+          <p className="mb-6 text-sm text-gray-400">
+            Sign in to your Premium design account
+          </p>
         </div>
-    );
-};
 
-export default LoginForm;
+        {/* form */}
+        <form onSubmit={handleSubmit(handleLogin)}>
+          {/* email */}
+          <div className="mb-6">
+            <div className="input-group ">
+              <input
+                type="email"
+                {...register("email", { required: "kamal@exmaple.com*" })}
+                placeholder=" "
+              />
+              <label>Email Address</label>{" "}
+            </div>
+            {errors.email && (
+              <p className="text-[12px] text-red-700 mt-2">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+
+          {/* password */}
+          <div className="mb-2">
+            <div className="input-group ">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder=" "
+                {...register("password", {
+                  required:
+                    "Must be at least 6 characters with 1 uppercase and 1 number*",
+                })}
+              />
+              <label>Password</label>
+              <span
+                className="absolute right-2 top-2 text-gray-600 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <Eye className="w-4.5" />
+                ) : (
+                  <EyeOff className="w-4.5" />
+                )}
+              </span>
+            </div>
+            {errors.password && (
+              <p className="text-[12px] text-red-700 mt-2">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          {/* forgot option */}
+          <p className="text-[12px] text-gray-200 underline hover:text-orange-600 mb-3">
+            <Link href="">Forgot password?</Link>
+          </p>
+
+          {/* submit button */}
+          <button
+            type="submit"
+            disabled={loading}
+            className="btn w-full  bg-linear-to-r
+                from-[#15508b]
+                via-[#1c6ab7]
+                to-orange-500 text-white mb-3   rounded-full disabled:cursor-not-allowed
+              disabled:opacity-70"
+          >
+            {loading ? (
+              <>
+                <p>
+                  Logging...
+                  <span className="loading loading-spinner w-4.5"></span>
+                </p>
+              </>
+            ) : (
+              "Login"
+            )}
+          </button>
+
+          {/* Google */}
+          <Button
+            variant="secondary"
+            className="w-full 
+            inset-0 hover:bg-linear-to-r
+                from-[#15508b]
+                via-[#1c6ab7]
+                to-orange-500 hover:text-white mb-3 border border-border rounded-full transition-all duration-500"
+            onPress={() => {
+              googleSignUp();
+              toast.info("Google signup coming soon! ", {
+                position: "top-right",
+                autoClose: 3000,
+              });
+            }}
+          >
+            <FcGoogle size={20} />
+            <span>Continue with Google</span>
+          </Button>
+
+          <p className="text-sm text-center text-gray-400 ">
+            Don&apos;t have an account?
+            <span className="text-orange-600">
+              <Link href={"/signup"}> Create one</Link>
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default LoginFrom;
